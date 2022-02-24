afb225085aa4788a4911e8f9de3875f44d080b0b
sunilskcj <sunilskcj@gmail.com>
2022-02-24 17:21:26 +0530

    tried calculation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,55 @@
 import { Component } from "@angular/core";
 
+
 @Component({
   selector: "app-main",
   templateUrl : "app.component.html",
   styleUrls : ["app.component.css"]
 })
 
-export class AppComponent{}   
+export class AppComponent{
+
+num1 : number = 0 ; 
+num2 :number = 0
+res : number = 0
+result : number = 0
+name : string = ""
+
+// shw(){
+//   console.log("clicked");
+  
+// }
+// show(val : string){
+//   console.log(val);
+
+//   this.name = val
+// }
+
+ add(a: number, b: number) {
+  this.res = a + b
+}
+
+ subtract(a: number, b: number) {
+  this.res = a - b
+}
+ multiply = (a: number, b: number) => {
+  this.res = a * b
+}
+
+ divide = (a: number, b: number) => this.res = a / b
+// constructor(){
+//   type calculateFnType = (m: number, n: number) => number
+// }
+show(){
+  this.result = this.res ;
+  
+}
+
+
+}
+
+
+//var testFn = (a: number, b: number, c: number) => a + b + c
+//calculate(t
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+  
